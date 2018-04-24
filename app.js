a2be56584f9c95d23d4916efd0207f2c92403a71
Jack Lye <catncradle@gmail.com>
2018-04-24 17:10:28 -0400

    got root working

--- app.js
+++ app.js
@@ -2,5 +2,34 @@ const express = require('express')
 const volleyball = require('volleyball')
 const bodyParser = require('body-parser')
 const nunjucks = require('nunjucks')
+const router = require('./routes')
+
 
 const app = express()
+
+
+//middleware
+app.use(volleyball)
+app.use(express.static('./public'))
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+const env = nunjucks.configure('views',{noCache:true}); // point nunjucks to the proper directory for templates
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(router)
+
+
+
+
+
+
+
+
+
+app.listen('3000',()=>{
+    console.log('listening on port 3000')
+})
+
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+let express = require('express')
+let router = express.Router()
+
+
+
+
+router.get('/',(req,res,next)=>{
+    res.render('index',{title:'Hope this works'})
+})
+
+
+module.exports = router
